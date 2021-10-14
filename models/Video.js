aac3fd719eae9f1a47e86378aeac77361f64da3a
sebastian garcia alvarez <sebasgarcia288@hotmail.com>
2021-10-14 13:22:51 -0500

    config model

--- models/Video.js
+++ models/Video.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { appConfig } = require('../config');
 const Schema = mongoose.Schema;
 
 const videosSchema = Schema({
@@ -19,9 +20,20 @@ const videosSchema = Schema({
         trim: true
     },
     tags:{
-        type: String,
+        type: String
+    },
+    videoURL:{
+        type:String
+    },
+    createdAt:{
+        type:Date, 
+        default: () => Date.now() - 5*60*60*1000
     }
-
 })
+
+videosSchema.methods.setVideoURL = function setVideoURL (filename) {
+    const {host, port} = appConfig
+    this.videoURL = `${host}:${port}/public/${filename}`
+}
 
 module.exports = mongoose.model('Video', videosSchema)
